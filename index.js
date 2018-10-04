afd23f5221b3459ddaa754749f941ea554e43609
Marcy Silverman <marcysilve225@gmail.com>
2018-10-03 20:42:34 -0700

    Javascript demo

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello bs preppers');
+var countTo=prompt ('how high should we count even numbers?');
+
+var sure=confirm('Are you sure you want to count to '+ countTo + '?');
+
+var x =12;
+if (x % 2 === 0) {
+    console.log('is even');
+}
+
+else {
+    console.log('is odd');
+}
+
+for (var i=0; i < 100; i++) {
+    var remainder = i % 2;
+    if (remainder === 0) {
+        console.log(i,'is even');
+        
+    }
+}
